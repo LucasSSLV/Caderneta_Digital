c33054ce4415c551a6f0283b76d63e6c60286339
LucasSSLV <lukinhas.kell@hotmail.com>
2025-11-04 21:36:39 -0300

    initial commit

---
+++ app/TelaInicial.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function TelaInicial() {
+
+    return (
+        <View>
+
+            <Text>Tela Inicial</Text>
+        </View>
+    )
+}
